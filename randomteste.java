0bf5e80972043e888fd67cc2fa4885c425df7dde
Nicolas Weiss Bueno <129218336+Bueno135@users.noreply.github.com>
2024-02-20 15:56:59 -0300

    randomteste.java

--- randomteste.java
+++ randomteste.java
@@ -1,3 +1,6 @@
+//Crie um programa em Python que implementa uma função que retorna uma lista preenchida com valores inteiros aleatórios. 
+//A função deve receber como parâmetro o tamanho da lista. O tamanho deve ser informado pelo usuário via input.
+
 import java.util.Random;
 import java.util.Scanner;
 import java.util.List;
