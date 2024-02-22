c106f8ea4470470073e5f92e399c0929b9c31497
Nicolas Weiss Bueno <129218336+Bueno135@users.noreply.github.com>
2024-02-22 11:47:26 -0300

    python1.py

--- python1.py
+++ python1.py
@@ -1,5 +1,8 @@
-#Crie um programa em Python que implementa uma função que retorna uma lista preenchida com valores inteiros aleatórios. 
-#A função deve receber como parâmetro o tamanho da lista. O tamanho deve ser informado pelo usuário via input.
+#Crie um programa em Python que implementa uma função que retorna uma lista preenchida com valores inteiros aleatórios.
+#Para cada número da lista, imprima:
+#1- Se o número é múltiplo de 3
+#2- Se o número é par
+#3- Se o número é ímpar
 import random
 
 tam = int(input("Digite qual será o tamanho da lista: "))
